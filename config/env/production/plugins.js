bae20a5aaf494c79242b2b83597e2088b843f1a2
Shaun Brown <shaun.brown@strapi.io>
2022-08-29 16:08:09 +0200

    plugins and middlware config update

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -6,6 +6,9 @@ upload: {
         accessKeyId: env('DO_SPACE_ACCESS_KEY'),
         secretAccessKey: env('DO_SPACE_SECRET_KEY'),
         endpoint: env('DO_SPACE_ENDPOINT'),
+        providerOptions: {
+            sizeLimit: 250 * 1024 * 1024 // 256mb in bytes
+          },
         params: {
           Bucket: env('DO_SPACE_BUCKET'),
         },
