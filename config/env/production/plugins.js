8f5d9c3818510710dd1415bd38b3f1791464ee7b
Shaun Brown <shaun.brown@strapi.io>
2022-08-31 17:49:51 +0200

    change provider

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,23 +1,27 @@
 module.exports = ({ env }) => ({
 upload: {
     config: {
-      provider: 'aws-s3',
+      provider: 'strapi-provider-upload-digitalocean-nohash',
       providerOptions: {
-        accessKeyId: env('DO_SPACE_ACCESS_KEY'),
-        secretAccessKey: env('DO_SPACE_SECRET_KEY'),
+        includeHash: true,
+        key: env('DO_SPACE_ACCESS_KEY'),
+        secret: env('DO_SPACE_SECRET_KEY'),
         endpoint: env('DO_SPACE_ENDPOINT'),
-        providerOptions: {
-            sizeLimit: 250 * 1024 * 1024 // 256mb in bytes
-          },
-        params: {
-          Bucket: env('DO_SPACE_BUCKET'),
-        },
+        space: env('DO_SPACE_BUCKET'),
+        directory: env('DO_SPACE_DIRECTORY'),
+        cdn: env('DO_SPACE_CDN'),
+        // providerOptions: {
+        //     sizeLimit: 250 * 1024 * 1024 // 256mb in bytes
+        //   },
+        // params: {
+        //   Bucket: env('DO_SPACE_BUCKET'),
+        // },
       },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
+    //   actionOptions: {
+    //     upload: {},
+    //     uploadStream: {},
+    //     delete: {},
+    //   },
     },
   },
 });
